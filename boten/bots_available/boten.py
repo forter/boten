317dcbe10140060b4875033f002e544f447f64b7
Moshe Zada <moshe@forter.com>
2015-11-24 20:25:22 +0200

    Some cleaning

--- boten/bots_available/boten.py
+++ boten/bots_available/boten.py
@@ -1,13 +1,9 @@
-# global params
-# * seilt
-# * verbose
-
 from core import BaseBot
 
 
 class Bot(BaseBot):
 
-    def command_ping(self, user_name, name="moshe"):
+    def command_ping(self, user_name):
         yield "pong {}".format(user_name)
 
     def command_reload(self):
