bffdef2c27bc5ce727f4c008ba6ad9dd4677fdeb
Moshe Zada <moshe@forter.com>
2015-12-05 23:03:17 +0200

    Add tox and travis support

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='boten',
     version='0.1',
-    packages=['boten'],
+    packages=find_packages(),
     url='',
     license='',
     author='Moshe Zada',
